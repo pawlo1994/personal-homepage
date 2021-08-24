67c5fd80703fb60147302eabb3056ea9c18122ab
pawlo4-94 <pawlo4-94@o2.pl>
2021-08-24 17:15:54 +0200

    store.js and themeSlice.js created

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { ThemeProvider } from "styled-components";
 import { Author } from "./Author";
 import GlobalStyle from "./common/globalStyle";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Author />
       </ThemeProvider>
-    </>
+    </Provider>
   );
 }
 
