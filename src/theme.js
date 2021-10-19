ba2a37d3ca79c6f897002618f8476dbad3f5efb7
pawlo4-94 <pawlo4-94@o2.pl>
2021-10-19 20:51:27 +0200

    breakpoint and more colors in dark theme added

--- src/theme.js
+++ src/theme.js
@@ -5,12 +5,23 @@ export const theme = {
         mineShaft: "#252525",
         slateGray: "#6E7E91",
         scienceBlue: "#0366D6",
+        buttonTextColor: "#FFFFFF",
+    },
+    breakpoint: {
+        mobileMax: "767px",
     }
 };
 
 export const dark = {
     color: {
         white: "#252525",
-        mineShaft: "FFFFFF",
+        whiteLilac: "#252525",
+        mineShaft: "#FFFFFF",
+        slateGray: "#FFFFFF",
+        scienceBlue: "#2188FF",
+        buttonTextColor: "#FFFFFF",
+    },
+    breakpoint: {
+        mobileMax: "767px",
     }
 };
