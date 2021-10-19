9080a36e7ac17d1275078e2f149865dff2849b96
pawlo4-94 <pawlo4-94@o2.pl>
2021-10-19 20:52:02 +0200

    darkModeSwitchLabel and darkModeSwitchWrapper added

--- src/features/author/DarkModeSwitch/index.js
+++ src/features/author/DarkModeSwitch/index.js
@@ -2,15 +2,18 @@ import { useSelector, useDispatch } from "react-redux";
 import darkThemeOff from "./darkThemeOff.svg";
 import darkThemeOn from "./darkThemeOn.svg";
 import { selectDarkMode } from "../../../themeSlice";
-import { StyledDarkModeSwitch } from "./styled";
+import { DarkModeSwitchLabel, DarkModeSwitchWrapper, StyledDarkModeSwitch } from "./styled";
 import { toggleDarkMode } from "../../../themeSlice";
 
 export const DarkModeSwitch = () => {
     const dispatch = useDispatch();
     const darkMode = useSelector(selectDarkMode);
     return (
-        <StyledDarkModeSwitch onClick={() => dispatch(toggleDarkMode())}>
-            <img src={darkMode ? darkThemeOn : darkThemeOff} alt="switch theme" />
-        </StyledDarkModeSwitch>
+        <DarkModeSwitchWrapper>
+            <DarkModeSwitchLabel>dark mode {darkMode === true ? "on" : "off"}</DarkModeSwitchLabel>
+            <StyledDarkModeSwitch onClick={() => dispatch(toggleDarkMode())}>
+                <img src={darkMode ? darkThemeOn : darkThemeOff} alt="switch theme" />
+            </StyledDarkModeSwitch>
+        </DarkModeSwitchWrapper>
     );
 };
--- src/features/author/DarkModeSwitch/styled.js
+++ src/features/author/DarkModeSwitch/styled.js
@@ -1,8 +1,26 @@
 import styled from "styled-components";
 
-export const StyledDarkModeSwitch = styled.button`
-    position: absolute;
+export const DarkModeSwitchLabel = styled.span`
+    color: ${({ theme }) => theme.color.slateGray};
+    font-size: 12px;
+    text-transform: uppercase;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax})
+    {
+        display: none;
+    }
+`;
+
+export const DarkModeSwitchWrapper = styled.div`
     display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: absolute;
+    right: 0;
+    top: 0;
+`
+
+export const StyledDarkModeSwitch = styled.button`
     align-items: center;
     justify-content: center;
     background-color: unset;
