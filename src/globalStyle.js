da558ff212aa3a6e64fbce95bb1502113bde3193
pawlo4-94 <pawlo4-94@o2.pl>
2021-10-28 20:27:32 +0200

    transition edited

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -4,7 +4,8 @@ const GlobalStyle = createGlobalStyle`
     body{
         background-color: ${({ theme }) => theme.color.whiteLilac};
         font-family: 'Inter', sans-serif;
-        transition: background-color 0.5s, color 1s;
+        transition-property: background-color, color;
+        transition-duration: 0.5s, 1s;
 
         @media(max-width: 767px){
             padding: 0;
