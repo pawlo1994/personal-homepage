696cae5fbfb08046db2937fea6416fd6770ebe62
pawlo4-94 <pawlo4-94@o2.pl>
2021-12-02 18:15:29 +0100

    dimentions and margin for mobiles edited

--- src/features/projects/Loading/styled.js
+++ src/features/projects/Loading/styled.js
@@ -3,8 +3,15 @@ import { ReactComponent as Spinner } from "./loadingSpinner.svg";
 
 export const LoadingSpinner = styled(Spinner)`
     margin-top: 48px;
+    width: 160px;
+    height: 160px;
     animation: spin 1s linear infinite;
 
+    @media(max-width:${({ theme }) => theme.breakpoint.mobileMax}){
+        margin-top: 24px;
+        width: 80px;
+        height: 80px;
+    }
     @keyframes spin{
         100%{
             transform: rotate(360deg);
@@ -19,4 +26,8 @@ export const LoadingWrapper = styled.span`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
+        margin-top: 10px;
+    }
 `;
